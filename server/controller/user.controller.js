1138465cd2f8b8bb8915d8c26f0dd6d69d6255a3
dominic <dominicseduraja3@gmail.com>
2024-01-02 18:36:34 +0530

    login user added controller

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,36 +1,37 @@
-const userModal = require('')
+// const userModal = require('')
 
 const userController={
  
     async createUser(req,res){
-        try{
-            let{
-                user_name,
-                user_email,
-                user_password,
-                user_about
-            }=req.body;
-
-        }
-        catch(err){
-            console.log("Error",err);
-        }
+        console.log("Haiiii",req.body);
+        // try{
+            // let{
+            //     user_name,
+            //     user_email,
+            //     user_password,
+            //     user_about
+            // }=req.body;
+            console.log("req body--->".req);
+        // }
+        // catch(err){
+        //     console.log("Error",err);
+        // }
     },
 
-    async loginUser(req,res){
-        try{
-            let{
-                user_name,
-                user_email,
-                user_password,
-                user_about
-            }=req.body;
+    // async loginUser(req,res){
+    //     try{
+    //         let{
+    //             user_name,
+    //             user_email,
+    //             user_password,
+    //             user_about
+    //         }=req.body;
 
-        }
-        catch(err){
-            console.log("Error",err);
-        }
-    },
+    //     }
+    //     catch(err){
+    //         console.log("Error",err);
+    //     }
+    // },
 }
 
 module.exports = userController;
--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const userRouter = require('./user.routes')
 
+router.use('/user',userRouter);
 
-module.export = router;
+module.exports = router;
