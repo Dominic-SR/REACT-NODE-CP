e76e72ce3bb7a1644838e1dcb00076b8675c1f0e
dominic <dominicseduraja3@gmail.com>
2024-01-27 19:28:15 +0530

    regitration completed

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const indexRouter = require('./routes/index.routes');
+const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
 
-console.log(`Your port is ${process.env.PORT}`);
-
+app.use(cors())
 app.use(express.json())
 app.use('/nest-billing',indexRouter)
 
-app.listen(5000,function(){
-    console.log("port running on 5000");
+app.listen(process.env.PORT,function(){
+    console.log(`port running on ${process.env.PORT}`);
 });
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,6 +3,7 @@ const database = require('../utils/database');
 const userModel = {
     async createUser(data){
         let query = `insert into users_table(user_name,user_email,user_mobileno,user_password,org_password,user_about) values ('${data.user_name}','${data.user_email}','${data.user_mobileno}','${data.ency_password}','${data.user_password}','${data.user_about}' )`;
+        console.log("QUERY",query);
         return database.promise().query(query)
     },
 
