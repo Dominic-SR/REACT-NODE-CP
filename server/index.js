fa32eea583d628b0da598372ba693a5b0068a8ef
dominic <dominicseduraja3@gmail.com>
2024-02-07 23:53:26 +0530

    chages added

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const indexRouter = require('./routes/index.routes');
 const cors = require('cors')
+const morgan = require('morgan')
 const dotenv = require('dotenv')
 dotenv.config()
 
 app.use(cors())
+app.use(morgan('dev'))
 app.use(express.json())
 app.use('/nest-billing',indexRouter)
 
